5124b485577a102bb3b6dc894df1ea6ff9a30562
Denis <denis.miroshnichenko.ukr@mail.ru>
2019-05-30 10:06:47 +0300

    source

---
+++ aes/stdafx.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <math.h>
+#include <iostream>
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <wmmintrin.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#define CPU_CLOCKS_NEEDED  CLOCKS_PER_SEC * 1000) / 3
